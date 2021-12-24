afaa9994de8161a2fb02246c4ddb0ea0904e48d8
khyunchoi <khyunchoi@naver.com>
2021-12-24 21:16:53 +0900

    Add 211224 jpashop section6

--- jpashop2/src/main/java/jpabook/jpashop2/JpaMain.java
+++ jpashop2/src/main/java/jpabook/jpashop2/JpaMain.java
@@ -1,5 +1,7 @@
 package jpabook.jpashop2;
 
+import jpabook.jpashop2.domain.Order;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -16,6 +18,10 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Order order = new Order();
+
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
