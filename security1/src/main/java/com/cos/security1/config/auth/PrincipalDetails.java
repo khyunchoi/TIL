5430563762eccd6c902b6d128cb5aa9e8c80e36d
khyunchoi <khyunchoi@naver.com>
2022-01-22 23:07:41 +0900

    Add 220122 TIL

---
+++ security1/src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
@@ -0,0 +1,64 @@
+package com.cos.security1.config.auth;
+
+// 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
+// 로그인 진행이 완료가 되면 Security session을 만들어줍니다. (Security ContextHolder)
+// 오브젝트 => Authentication 타입 객체
+// Authentication 안에 User 정보가 있어야 됨.
+// User 오브젝트 타입 => UserDetails 타입 객체
+
+// Security Session => Authentication => UserDetails
+
+import com.cos.security1.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetails(User user) {
+        super();
+        this.user = user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    // 해당 User의 권한을 리턴하는 곳!!
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collet = new ArrayList<GrantedAuthority>();
+        collet.add(()->{ return user.getRole();});
+        return collet;
+    }
+}
